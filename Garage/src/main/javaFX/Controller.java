ee944b0289dd6a54474e90de4ebd91693bdf5f0d
Reinoud van Zoelen <Reinoud van Zoelen>
2017-03-10 12:00:39 +0100

    Added functionality to draw the car (now in rectangle form)

--- Garage/src/main/javaFX/Controller.java
+++ Garage/src/main/javaFX/Controller.java
@@ -2,10 +2,11 @@ package main.javaFX;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.canvas.Canvas;
+import javafx.geometry.Bounds;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.Pane;
 import main.models.Auto;
 import main.models.Garage;
 
@@ -13,16 +14,38 @@ public class Controller {
 
     private Garage garage;
     private GraphicsContext gc;
-
     @FXML
-    private Canvas ToPaintOn;
-
+    private Pane backgroundPane;
     @FXML
     private ListView viezeAutoLijst;
+
+
 
     @FXML
     public void buttonPressed(ActionEvent event) {
         System.out.println("Button has been clicked.");
+
+        _moveCar(event);
+
+    }
+
+    private void _paintCar(ActionEvent event, Auto auto) {
+        DrawingController draw = new DrawingController(backgroundPane);
+
+        // Get the button so we can know where to draw underneath
+        Button source = (Button) event.getSource();
+        Bounds boundsInScene = source.localToScene(source.getBoundsInLocal());
+
+        // Find the action positions where we want to draw the car
+        int xTruePosition = (int) boundsInScene.getMaxX() - (int) boundsInScene.getWidth();
+        int yTruePosition = (int) boundsInScene.getMaxY() + (int) boundsInScene.getHeight();
+
+        draw.DrawCar(xTruePosition, yTruePosition, auto);
+
+        System.out.println("Car has been painted");
+    }
+
+    private void _moveCar(ActionEvent event) {
         if (viezeAutoLijst.getSelectionModel().getSelectedIndex() >= 0) {
             Auto auto = (Auto) viezeAutoLijst.getItems().get(viezeAutoLijst.getSelectionModel().getSelectedIndex());
 
@@ -31,8 +54,11 @@ public class Controller {
             button.setText(auto.toString());
 
             viezeAutoLijst.getItems().remove(viezeAutoLijst.getSelectionModel().getSelectedIndex());
-        }
 
+            System.out.println("Car has been moved from the list");
+
+            _paintCar(event, auto);
+        }
     }
 
     public void Restart() {
--- Garage/src/main/javaFX/DrawingController.java
+++ Garage/src/main/javaFX/DrawingController.java
@@ -1,6 +1,8 @@
 package main.javaFX;
 
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.*;
+import main.models.Auto;
 
 public class DrawingController {
 
@@ -10,8 +12,13 @@ public class DrawingController {
         this.pane = pane;
     }
 
-    public void DrawCar(double location) {
+    public void DrawCar(int xPosition, int yPosition, Auto auto) {
 
+
+        Rectangle body = new Rectangle(xPosition, yPosition, 120, 40);
+
+        Line line = new Line(xPosition, yPosition, xPosition + 100, yPosition+100);
+
+        pane.getChildren().add(body);
     }
-
 }
